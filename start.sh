b308a4b091d9974653ccb37aaf1abd961df6dd6a
botts3 <57943437+botts3@users.noreply.github.com>
2019-11-19 16:17:54 +0100

    Fixed start

--- start.sh
+++ start.sh
@@ -9,50 +9,50 @@
 	function start {
 		if ! screen -list | grep -q "botphp1"; then
 			screen -AdmS botphp1 php bot.php -i 1
-			text "32;01m" "Pomyslnie uruchomiono bota nr 1!"
+			text "32;01m" "Successfully launched bot 1!"
 		else
-			text "31;02m" "Bot nr 1 jest juz uruchomiony!"
+			text "31;02m" "Bot 1 is already running!"
 		fi
 		if ! screen -list | grep -q "botphp2"; then
 			screen -AdmS botphp2 php bot.php -i 2
-			text "32;01m" "Pomyslnie uruchomiono bota nr 2!"
+			text "32;01m" "Bot 2 started successfully!"
 		else
-			text "31;02m" "Bot nr 2 jest juz uruchomiony!"
+			text "31;02m" "Bot 2 is already running!"
 		fi
 	}
 
 	function stop {
 		if ! screen -list | grep -q "botphp1"; then
-			text "31;02m" "Bot nr 1 nie był uruchomiony więc nie został zatrzymany"
+			text "31;02m" "Bot 1 was not running so it was not stopped"
 		else
-			text "32;01m" "Pomyslnie zatrzymano bota  nr 1!"
+			text "32;01m" "Bot # 1 was successfully stopped!"
 			screen -X -S botphp1 stuff "^C"
 		fi
 		if ! screen -list | grep -q "botphp2"; then
-			text "31;02m" "Bot nr 2 nie był uruchomiony więc nie został zatrzymany"
+			text "31;02m" "Bot No. 2 was not running so it was not stopped"
 		else
-			text "32;01m" "Pomyslnie zatrzymano bota nr 2!"
+			text "32;01m" "Bot bot has been stopped successfully 2!"
 			screen -X -S botphp2 stuff "^C"
 		fi
 	}
 
 	function restart {
 		if ! screen -list | grep -q "botphp1"; then
-			text "31;02m" "Bot nr 1 nie był uruchomiony"
+			text "31;02m" "Bot 1 was not running"
 		else
-			text "32;01m" "Pomyslnie zatrzymano bota nr 1!"
+			text "32;01m" "Bot bot has been stopped successfully1!"
 			screen -X -S botphp1 stuff "^C"
 		fi
 		if ! screen -list | grep -q "botphp2"; then
-			text "31;02m" "Bot nr 2 nie był uruchomiony"
+			text "31;02m" "Bot No. 2 was not running"
 		else
-			text "32;01m" "Pomyslnie zatrzymano bota nr 2!"
+			text "32;01m" "Bot bot has been stopped successfully 2!"
 			screen -X -S botphp2 stuff "^C"
 		fi
 		screen -AdmS botphp1 php bot.php -i 1
-		text "32;01m" "Pomyslnie uruchomiono bota nr 1!"
+		text "32;01m" "Successfully launched bot no 1!"
 		screen -AdmS botphp2 php bot.php -i 2
-		text "32;01m" "Pomyslnie uruchomiono bota nr 2!"
+		text "32;01m" "Successfully launched bot no 2!"
 	}
 
 	function update {
@@ -60,19 +60,8 @@
 	}
 
 	clear
-	text "32;01m" "
- _______ _____ ____    ____        _     _             __  __       _                 
-|__   __/ ____|___ \  |  _ \      | |   | |           |  \/  |     (_)                
-   | | | (___   __) | | |_) | ___ | |_  | |__  _   _  | \  / | __ _ _  ___ ___  _ __  
-   | |  \___ \ |__ <  |  _ < / _ \| __| | '_ \| | | | | |\/| |/ \` | |/ __/ _ \| '_ \ 
-   | |  ____) |___) | | |_) | (_) | |_  | |_) | |_| | | |  | | (_| | | (_| (_) | | | |
-   |_| |_____/|____/  |____/ \___/ \__| |_.__/ \__, | |_|  |_|\__,_| |\___\___/|_| |_|
-                                                __/ |             _/ |                
-                                               |___/             |__/                 
-	"
-	text "32;01m" "Jeżeli chcesz mnie wspomóc możesz wysłać donejta ;)
-	Paypal: https://www.paypal.me/Majcon
-	Inne płatności: https://tipanddonation.com/majcon"
+	text "32;01m" "BOT-TS3.COM"
+	text "32;01m" ""
 	
 	case "$1" in
 		"start")
@@ -92,7 +81,7 @@
 		;;
 
 		*)
-			echo -e 'Uzyj start | stop | restart'
+			echo -e 'Use start | stop | restart'
 		;; 
 	esac
 
